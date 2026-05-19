931c055a74bdeaef6e3ed72556d2c7fa4e17388d
Waxmatuct <alvheim@rambler.ru>
2026-05-20 00:05:10 +0300

    интеграция с vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import vue from '@astrojs/vue';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [vue()],
+  i18n: {
+    defaultLocale: "ru",
+    locales: ["ru", "en"],
+    routing: {
+      prefixDefaultLocale: true,
+    }
+  }
+});
